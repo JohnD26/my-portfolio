590956dae7d7c699d58806d2def3cbb9301e1fcd
JohnD26 <dominguejonathan@gmail.com>
2025-05-25 23:08:01 -0400

    Modifs+ Cleanup

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { NavBar } from "./components/Navbar";
-// import { Banner } from "./components/Banner";
-// import { Projects } from "./components/Projects";
-// import { Contact } from "./components/Contact";
-// import { Footer } from "./components/Footer";
-// import { Design1 } from "./pages/Design1";
-// import { Design2 } from "./pages/Design2";
-// import { Design3 } from "./pages/Design3";
-// import { Design4 } from "./pages/Design4";
-
-// function App() {
-//   return (
-    
-//     <div className="App">
-//       <NavBar />
-//       <Banner />
-//       <Projects />
-//       <Contact />
-//       <Footer />
-//     </div>
-
-    
-//   );
-// }
-
-
-// export default App;
-
-
-
-// src/App.js
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Use HashRouter so GitHub Pages always serves index.html
+
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { NavBar }   from './components/Navbar';
@@ -65,7 +31,7 @@ function App() {
           </>
         }/>
 
-        {/* Stub pages for your 4 designs */}
+        {/* Stub pages for  4 designs */}
         <Route path="/design1" element={<Design1 />} />
         <Route path="/design2" element={<Design2 />} />
         <Route path="/design3" element={<Design3 />} />
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -142,44 +142,6 @@ export const Banner = () => {
     return null;
   }
 
-  // 3. display
-//   return (
-//     <section className="banner" id="home">
-//       <Container>
-//         <Row className="aligh-items-center">
-//           <Col xs={12} md={6} xl={7}>
-//             <TrackVisibility>
-//               {({ isVisible }) =>
-//                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-//                   <span className="tagline">Welcome to my Portfolio</span>
-//                   <h1>
-//                     {`Hi! I'm Jonathan`}{" "}
-//                     <span
-//                       className="txt-rotate"
-//                       dataPeriod="1000"
-//                       data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
-//                     >
-//                       <span className="wrap">{text}</span>
-//                     </span>
-//                   </h1>
-//                   <p>I'm new to web Design/UI Conception and learning it for a course</p>
-//                 </div>
-//               }
-//             </TrackVisibility>
-//           </Col>
-//           <Col xs={12} md={6} xl={5}>
-//             <TrackVisibility>
-//               {({ isVisible }) =>
-//                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-//                   <img src={headerImg} alt="Header Img" />
-//                 </div>
-//               }
-//             </TrackVisibility>
-//           </Col>
-//         </Row>
-//       </Container>
-//     </section>
-//   );
   return (
     <section className="banner" id="home">
       <Container>
