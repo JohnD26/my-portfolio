1934433000028dc7fb7dd001d2cc3a5fd825574e
JohnD26 <dominguejonathan@gmail.com>
2025-05-25 21:37:25 -0400

    Adding routers-hash

--- src/App.js
+++ src/App.js
@@ -1,30 +1,87 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { NavBar } from "./components/Navbar";
+// import { Banner } from "./components/Banner";
+// import { Projects } from "./components/Projects";
+// import { Contact } from "./components/Contact";
+// import { Footer } from "./components/Footer";
+// import { Design1 } from "./pages/Design1";
+// import { Design2 } from "./pages/Design2";
+// import { Design3 } from "./pages/Design3";
+// import { Design4 } from "./pages/Design4";
+
+// function App() {
+//   return (
+    
+//     <div className="App">
+//       <NavBar />
+//       <Banner />
+//       <Projects />
+//       <Contact />
+//       <Footer />
+//     </div>
+
+    
+//   );
+// }
+
+
+// export default App;
+
+
+
+// src/App.js
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/Navbar";
-import { Banner } from "./components/Banner";
-import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
-import { Design1 } from "./pages/Design1";
-import { Design2 } from "./pages/Design2";
-import { Design3 } from "./pages/Design3";
-import { Design4 } from "./pages/Design4";
+
+// Use HashRouter so GitHub Pages always serves index.html
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { NavBar }   from './components/Navbar';
+import { Banner }   from './components/Banner';
+import { Projects } from './components/Projects';
+import { Contact }  from './components/Contact';
+import { Footer }   from './components/Footer';
+
+import { Design1 }  from './pages/Design1';
+import { Design2 }  from './pages/Design2';
+import { Design3 }  from './pages/Design3';
+import { Design4 }  from './pages/Design4';
 
 function App() {
   return (
-    
-    <div className="App">
+    <Router>
       <NavBar />
-      <Banner />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
 
-    
+      <Routes>
+        {/* Home route: banner + projects + contact */}
+        <Route path="/" element={
+          <>
+            <Banner />
+            <Projects />
+            <Contact />
+          </>
+        }/>
+
+        {/* Stub pages for your 4 designs */}
+        <Route path="/design1" element={<Design1 />} />
+        <Route path="/design2" element={<Design2 />} />
+        <Route path="/design3" element={<Design3 />} />
+        <Route path="/design4" element={<Design4 />} />
+
+        {/* Catch-all 404 */}
+        <Route path="*" element={
+          <div style={{ padding:'2rem', textAlign:'center' }}>
+            <h2>Page non trouvée</h2>
+          </div>
+        }/>
+      </Routes>
+
+      <Footer />
+    </Router>
   );
 }
-
 
 export default App;
--- src/pages/Design4.js
+++ src/pages/Design4.js
@@ -1,7 +1,11 @@
 import React from 'react';
-export const Design4 = () => (
-  <div className="design-page">
-    <h1>Design 4 : En Developement</h1>
-    <p>Ici viendra la maquette complète…</p>
-  </div>
-);
+import { Container } from 'react-bootstrap';
+
+export const Design4 = () => {
+  return (
+    <Container style={{ padding: '4rem 0', textAlign: 'center' }}>
+      <h1>Design 1 : Chez le coiffeur</h1>
+      <p>Page à venir…</p>
+    </Container>
+  );
+};
