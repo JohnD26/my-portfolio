433d7a86248ccfba77615cbbd9c7b200cbd25925
JohnD26 <dominguejonathan@gmail.com>
2025-05-25 21:09:33 -0400

    Attempt to fix issue with router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/Navbar";
-import { Banner } from "./components/Banner";
-import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
-import { Design1 } from "./pages/Design1";
-import { Design2 } from "./pages/Design2";
-import { Design3 } from "./pages/Design3";
-import { Design4 } from "./pages/Design4";
+// import logo from './logo.svg';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { NavBar } from "./components/Navbar";
+// import { Banner } from "./components/Banner";
+// import { Projects } from "./components/Projects";
+// import { Contact } from "./components/Contact";
+// import { Footer } from "./components/Footer";
+// import { Design1 } from "./pages/Design1";
+// import { Design2 } from "./pages/Design2";
+// import { Design3 } from "./pages/Design3";
+// import { Design4 } from "./pages/Design4";
 
 // function App() {
 //   return (
@@ -25,6 +25,19 @@ import { Design4 } from "./pages/Design4";
     
 //   );
 // }
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { NavBar }   from "./components/Navbar";
+import { Banner }   from "./components/Banner";
+import { Projects } from "./components/Projects";
+import { Contact }  from "./components/Contact";
+import { Footer }   from "./components/Footer";
+import { Design1 }  from "./pages/Design1";
+import { Design2 }  from "./pages/Design2";
+import { Design3 }  from "./pages/Design3";
+import { Design4 }  from "./pages/Design4";
 
 function App() {
   return (
